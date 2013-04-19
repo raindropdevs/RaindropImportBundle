09ad718b05ae18b8172414a658a30b58b26e49bf
Edoardo Rivello <edoardo.rivello@gmail.com>
2013-04-19 16:27:10 +0200

    add csv reader

---
+++ Tests/Util/ReaderTest.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace Raindrop\ImportBundle\Tests\Util;
+
+use Raindrop\ImportBundle\Util\Reader;
+
+class ReaderTest extends \PHPUnit_Framework_TestCase
+{
+    protected $reader;
+
+    public function setUp()
+    {
+        $this->reader = new Reader();
+        $this->reader->open(__DIR__ . '/../Fixtures/import.csv');
+    }
+
+    public function testGetHeaders()
+    {
+        $this->assertEquals(
+            array(
+                0 => 'Header 1',
+                1 => 'Header 2',
+                2 => 'Header 3',
+            ),
+            $this->reader->getHeaders()
+        );
+    }
+
+    public function testGetRow()
+    {
+        $this->assertEquals(
+            array(
+                0 => 'row1column1',
+                1 => 'row1column2',
+                2 => 'row1column3',
+            ),
+            $this->reader->getRow()
+        );
+        $this->assertEquals(
+            array(
+                0 => 'row2column1',
+                1 => 'row2column2',
+                2 => 'row2column3',
+            ),
+            $this->reader->getRow()
+        );
+        $this->assertEquals(
+            array(
+                0 => 'row3column1',
+                1 => 'row3column2',
+                2 => 'row3column3',
+            ),
+            $this->reader->getRow()
+        );
+    }
+
+    public function testGetRows()
+    {
+        $this->assertEquals(
+            array(
+                0 => array(
+                    0 => 'row1column1',
+                    1 => 'row1column2',
+                    2 => 'row1column3',
+                ),
+                1 => array(
+                    0 => 'row2column1',
+                    1 => 'row2column2',
+                    2 => 'row2column3',
+                ),
+            ),
+            $this->reader->getRows(2)
+        );
+    }
+
+    public function testGetAll()
+    {
+        $this->assertEquals(3, count($this->reader->getAll()));
+    }
+}
---
+++ Util/Reader.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace Raindrop\ImportBundle\Util;
+
+/**
+ * Read a CSV file
+ */
+class Reader
+{
+    protected $handle;
+    protected $delimiter;
+    protected $enclosure;
+    protected $line;
+    protected $headers;
+
+    /**
+     * Open a CSV file
+     *
+     * @param string  $file       The file path
+     * @param string  $delimiter  The CSV's delimiter
+     * @param string  $mode       fopen mode
+     * @param string  $enclosure  The enclosure
+     * @param boolean $hasHeaders Does the CSV have any headers?
+     */
+    public function open($file, $delimiter = ',', $mode = 'r+', $enclosure = '"', $hasHeaders = true)
+    {
+        $this->handle = fopen($file, $mode);
+        $this->delimiter = $delimiter;
+        $this->enclosure = $enclosure;
+        $this->line = 0;
+
+        if ($hasHeaders) {
+            $this->headers = $this->getRow();
+        }
+    }
+
+    /**
+     * Return a row
+     *
+     * @return array or false
+     */
+    public function getRow()
+    {
+        if (($row = fgetcsv($this->handle, 1000, $this->delimiter, $this->enclosure)) !== false) {
+            $this->line++;
+
+            return $row;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Get an array of rows
+     *
+     * @param int $count The number of rows to return
+     *
+     * @return array of row
+     */
+    public function getRows($count)
+    {
+        $rows = array();
+        for ($i=0; $i < $count; $i++) {
+            $row = $this->getRow();
+            if ($row) {
+                $rows[] = $row;
+            }
+        }
+
+        return $rows;
+    }
+
+    /**
+     * Return entire table
+     *
+     * @return array $data
+     */
+    public function getAll()
+    {
+        $data = array();
+        while ($row = $this->getRow()) {
+            $data[] = $row;
+        }
+
+        return $data;
+    }
+
+    /**
+     * Get headers
+     *
+     * @return array
+     */
+    public function getHeaders()
+    {
+        return $this->headers;
+    }
+
+    /**
+     * Close file
+     */
+    public function __destruct()
+    {
+        if (is_resource($this->handle)) {
+            fclose($this->handle);
+        }
+    }
+
+}
